bac1396eb6979339ce3c7908245adbd223a98c89
Corina Ionelia <corynaionelia@gmail.com>
2024-10-07 17:29:00 +0300

    3

---
+++ my-contact-book/src/components/ContactForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addContact } from '../redux/contactsSlice';
+import styles from './ContactForm.module.css';
+
+const ContactForm = () => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newContact = {
+      id: Date.now(),
+      name,
+      number,
+    };
+    dispatch(addContact(newContact));
+    setName('');
+    setNumber('');
+    alert(`${name} has been added! 📞`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Number:
+        <input
+          type="tel"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+};
+
+export default ContactForm;
